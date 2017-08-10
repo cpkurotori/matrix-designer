92996e5cf69891272be3796675182ea209225121
Cameron Kurotori <spammycammy95@gmail.com>
2017-08-10 21:55:54 +0000

    load images using a textarea

--- js/matrix.js
+++ js/matrix.js
@@ -1,9 +1,9 @@
 /* global $*/
 
 function changeColor(){
-    var red = $('input')[1].value;
-    var green =$('input')[2].value;
-    var blue = $('input')[3].value;
+    var red = $('input')[3].value;
+    var green =$('input')[4].value;
+    var blue = $('input')[5].value;
     var rgb = String(red)+','+String(green)+','+String(blue);
     console.log(rgb);
     if (!this.active || this.rgb != rgb){
@@ -18,10 +18,40 @@ function changeColor(){
     };
 };
 
+function fillAll(){
+    var width = document.getElementById('width').value
+    var height = document.getElementById('height').value
+    var red = $('input')[3].value;
+    var green =$('input')[4].value;
+    var blue = $('input')[5].value;
+    var rgb = String(red)+','+String(green)+','+String(blue);   
+    for (var i = 0; i < height; i++){
+        for (var j = 0; j < width; j++){
+            var cell = document.getElementById(width*Number(i)+Number(j));
+            cell.style="background-color:rgb("+rgb+");";
+            cell.active = true;
+            cell.rgb = rgb;
+        };
+    };
+};
+
+function clearBack(){
+    var width = document.getElementById('width').value
+    var height = document.getElementById('height').value
+    for (var i = 0; i < height; i++){
+        for (var j = 0; j < width; j++){
+            var cell = document.getElementById(width*Number(i)+Number(j));
+            cell.style="background-color:rgb(0,0,0);";
+            cell.active = false;
+            cell.rgb = "0,0,0";
+        };
+    };
+};
+
 function hoverColor(){
-    var red = $('input')[1].value;
-    var green =$('input')[2].value;
-    var blue = $('input')[3].value;
+    var red = $('input')[3].value;
+    var green =$('input')[4].value;
+    var blue = $('input')[5].value;
     var rgb = String(red)+','+String(green)+','+String(blue);
     this.style="background-color:rgb("+rgb+");";
     if (this.active && this.rgb == rgb){
@@ -41,67 +71,107 @@ function revertColor(){
     this.style="background-color:rgb("+this.rgb+");";
 }
 
-var table = document.getElementById('matrix_table');
-for (var i = 0; i < 8; i++){
-    var tr = document.createElement("TR");
-    for (var j = 0; j < 8; j++){
-        var td = document.createElement("TD");
-        td.id = String(i)+'-'+String(j)
-        td.rgb = "0,0,0";
-        td.active = false;
-        td.style="background-color:rgb(0,0,0);"
-        td.onclick = changeColor;
-        td.onmouseover = hoverColor;
-        td.onmouseout = revertColor;
-        tr.appendChild(td);
-    };
-    table.appendChild(tr);
-};
 
+function createTable(){
+    var table = document.getElementById('matrix_table');
+    while (table.hasChildNodes()){
+        table.removeChild(table.lastChild);
+    }
+    var width=document.getElementById("width").value;
+    var height=document.getElementById("height").value;
+    if (width > 16) {width=16;document.getElementById("width").value=16};
+    if (height > 16) {height=16;document.getElementById("height").value=16};
+    if (width < 1) {width=1;document.getElementById("width").value=1};
+    if (width < 1) {height=1;document.getElementById("height").value=1};
+    for (var i = 0; i < height; i++){
+        var tr = document.createElement("TR");
+        for (var j = 0; j < width; j++){
+            var td = document.createElement("TD");
+            td.id = width*Number(i)+Number(j);
+            td.rgb = "0,0,0";
+            td.active = false;
+            td.style="background-color:rgb(0,0,0);";
+            td.onclick = changeColor;
+            td.onmouseover = hoverColor;
+            td.onmouseout = revertColor;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+}
 
 
 
 function printGrid(colors,matrix){
-    var list = document.getElementById('color-list');
-    while (list.hasChildNodes()){
-        list.removeChild(list.lastChild);
-    }
+    // var list = document.getElementById('color-list');
+    // while (list.hasChildNodes()){
+    //     list.removeChild(list.lastChild);
+    // }
+    var text = 'width = '+document.getElementById('width').value+'\nheight = '+document.getElementById('height').value+'\n';
     for (var col in Object.keys(colors)){
-        var li = document.createElement('LI')
-        var textnode = document.createTextNode(colors[Object.keys(colors)[col]]+" = "+'('+Object.keys(colors)[col]+')');
-        li.appendChild(textnode);
-        list.appendChild(li);
+        // var li = document.createElement('LI')
+        // var textnode = document.createTextNode(colors[Object.keys(colors)[col]]+" = "+'('+Object.keys(colors)[col]+')');
+        // li.appendChild(textnode);
+        // list.appendChild(li);
+        text += colors[Object.keys(colors)[col]]+" = "+'('+Object.keys(colors)[col]+')';
+        text += '\n'
     };
-    var matrix_text = document.createElement('LI');
-    var textnode = document.createTextNode("image = ["+String(matrix)+"]");
-    matrix_text.appendChild(textnode);
-    list.appendChild(matrix_text)
+    // var matrix_text = document.createElement('LI');
+    // var textnode = document.createTextNode("image = ["+String(matrix)+"]");
+    text += "image = ["+String(matrix)+"]"
+    // matrix_text.appendChild(textnode);
+    // list.appendChild(matrix_text)
+    document.getElementById('load').value = text;
 };
 
 function populateMatrix(){
-    var alpha = "abcdefghijklmnopqrstuvwxyz".split('')
+    var width = document.getElementById('width').value;
+    var height = document.getElementById('height').value;
+    var alpha = "abcdefghijklmnopqrstuvwxyz".split('');
     var colorList = {};
     var matrix = [];
     var colorCount = 0;
-    for (var i = 0; i < 8; i++){
-        for (var j = 0; j < 8; j++){
-            var color = document.getElementById(String(i)+'-'+String(j)).rgb;
+    for (var i = 0; i < height; i++){
+        for (var j = 0; j < width; j++){
+            var color = document.getElementById(width*Number(i)+Number(j)).rgb;
             if (colorList[color] === undefined) {
                 colorList[color] = alpha[colorCount];
                 colorCount ++;
-            };
+            }
             matrix.push(colorList[color]);
         }
     }
     printGrid(colorList,matrix);
 }
 
-// $(document).ready(function(){
-//     $("input").change(function(){
-//         var red = document.getElementById("red").value;
-//         var green = document.getElementById("green").value;
-//         var blue = document.getElementById("blue").value;
-//         var rgb = "("+String(red)+','+String(green)+','+String(blue)+")";
-//         document.getElementById("sample").style = "background-color:rgb"+rgb;
-//     });
-// });
+function loadImage(){
+    document.getElementById('error').innerHTML = '';
+    try {
+        var dict = {}
+        var text = document.getElementById('load').value.split('\n');
+        for (var entry in text){
+            var t = text[entry].split('=');
+            dict[t[0].substr(0,t[0].search(' '))+t[0].substr(t[0].search(' ')+1,t[0].length)] = t[0].substr(0,t[1].search(' '))+t[1].substr(t[1].search(' ')+1,t[1].length)
+        }
+        var image = dict['image'].split(',');
+        image[0] = image[0][1];
+        image[image.length-1] = image[image.length-1][0];
+        document.getElementById('width').value= Number(dict['width']);
+        document.getElementById('height').value= Number(dict['height']);
+        createTable()
+        for (var i in image){
+            var td = document.getElementById(i);
+            var rgb = dict[image[i]].substr(1,dict[image[i]].length-2);
+            td.style="background-color:"+"rgb("+rgb+");";
+            td.active=true;
+            td.rgb=rgb;
+        }
+    }
+    catch(err){
+        document.getElementById('error').innerHTML = 'Incorrect format of loaded image... try again!';
+        console.log('Incorrect format of loaded image...');
+    }
+}
+
+
+loadImage()
